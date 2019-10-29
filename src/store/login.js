035d275d10df59713835a6594548a1b717b3848c
yxy-start <15282716136@163.com>
2019-10-29 16:22:32 +0800

    login

--- src/store/login.js
+++ src/store/login.js
@@ -1,20 +1,33 @@
-import {get} from '../http/axios'
+import {get,post,post_json} from '../http/axios'
 export default {
   namespaced:true,
   state:{
-    ues:[]
+    token:"",
   },
   mutations:{
-    refreshUes(state,ues){
-      state.ues = ues;
-      console.log(ues);
+    refreshUes(state,token){
+      state.token = token.token;
+      console.log("token",token);
     }
   },
   actions:{
-    async findUes(context){
-      let response = await get("/user/info");
-      context.commit("refreshUes",response.data)
-      console.log(response.data);
+    async findUer(context,customer){
+      let response = await post_json("/user/login",customer);
+      if(response.data==null){
+        response="登陆失败"
+       
+      }else{
+        context.commit("refreshUes",response.data)
+      }
+     
+      // console.log(response.data);
+      return response;
+    },
+    async TokenInfo(context,token){
+      let response=await get("/user/info?token="+token);
+      // context.dispatch("findUer",response.data)
+      return response.data;
     }
   }
+
 }
